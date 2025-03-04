c24f07c2aa139e5c65d3897991d3aa9864b66969
KiwiKetil <ketilsveberg@gmail.com>
2025-03-04 10:02:38 +0100

    finished unittests for GetUsersAsync()

--- tests/RoomSchedulerAPI.UnitTests/ServicesTests/UserServiceTests.cs
+++ tests/RoomSchedulerAPI.UnitTests/ServicesTests/UserServiceTests.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using Microsoft.Extensions.Logging;
 using Moq;
+using MySqlX.XDevAPI.CRUD;
 using RoomSchedulerAPI.Features.AutoMapper;
 using RoomSchedulerAPI.Features.Models.DTOs.UserDTOs;
 using RoomSchedulerAPI.Features.Models.Entities;
@@ -30,7 +31,7 @@ public class UserServiceTests
         IMapper mapper = mapperConfig.CreateMapper();
 
         _userService = new UserService(
-            _userRepositoryMock.Object, 
+            _userRepositoryMock.Object,
             _userRoleRepositoryMock.Object,
             _passwordVerificationServiceMock.Object,
             _passwordHistoryRepositoryMock.Object,
@@ -40,26 +41,61 @@ public class UserServiceTests
             );
     }
 
+    #region GetUsersAsync
+
     [Fact]
-    public async Task GetUsersAsync_ReturnsUsersWithCountDTO() 
+    public async Task GetUsersAsync_ReturnsUsersWithCountDTO_WithValidData()
     {
         // Arrange
-        var userQuery = new UserQuery( null, null, null, null );
+        var userQuery = new UserQuery(null, null, null, null);
 
         List<User> users = [
-            new User { FirstName = "Jim" },
-            new User { FirstName = "Michelle" }
+            new User { FirstName = "Jim", LastName = "Moore" },
+            new User { FirstName = "Michelle", LastName = "Andersson" }
             ];
-        int totalCount = 10;
+        int totalCount = users.Count;
 
-        _userRepositoryMock.Setup(x => x.GetUsersAsync(userQuery)).ReturnsAsync((users, totalCount));
+        _userRepositoryMock.Setup(x => x.GetUsersAsync(userQuery))
+            .ReturnsAsync((users, totalCount));
 
         //Act
         var result = await _userService.GetUsersAsync(userQuery);
 
         //Assert
         var usersWithCountDTO = Assert.IsType<UsersWithCountDTO>(result);
-        Assert.Equal(totalCount, usersWithCountDTO.TotalCount);        
-        Assert.Equal(usersWithCountDTO.UserDTOs.First().FirstName, users[0].FirstName);        
+        Assert.Equal(totalCount, usersWithCountDTO.TotalCount);
+        Assert.Equal(totalCount, usersWithCountDTO.UserDTOs.Count());
+        Assert.Equal(usersWithCountDTO.UserDTOs.ElementAt(0).FirstName, users[0].FirstName);
+        Assert.Equal(usersWithCountDTO.UserDTOs.ElementAt(0).LastName, users[0].LastName);
+        Assert.Equal(usersWithCountDTO.UserDTOs.ElementAt(1).FirstName, users[1].FirstName);
+        Assert.Equal(usersWithCountDTO.UserDTOs.ElementAt(1).LastName, users[1].LastName);
     }
+
+    [Fact]
+    public async Task GetUsersAsync_WhenNoUsersAreFound_ReturnsUsersWithCountDTO_WithEmptyCollectionAndTotalCountZero() 
+    {
+        // Arrange
+        var query = new UserQuery(null, null, null, null);
+
+        List<User> users = [];
+        int totalCount = users.Count;
+
+        _userRepositoryMock.Setup(x => x.GetUsersAsync(query)).ReturnsAsync((users, totalCount));
+
+        // Act
+        var result = await _userService.GetUsersAsync(query);
+
+        // Assert
+        var userWithCountDTO = Assert.IsType<UsersWithCountDTO>(result);
+        Assert.Empty(userWithCountDTO.UserDTOs);
+        Assert.Equal(0, userWithCountDTO.TotalCount);
+    }
+
+
+
+
+
+
+    #endregion GetUsersAsync
+
 }
