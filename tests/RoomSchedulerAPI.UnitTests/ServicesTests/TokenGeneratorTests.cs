97c24a9d5fcffc4b37c991c508857e30869067ed
KiwiKetil <ketilsveberg@gmail.com>
2025-03-05 15:57:31 +0100

    updated unittests TokenGeneratorTests to Theory and AutoFixture

--- tests/RoomSchedulerAPI.UnitTests/ServicesTests/TokenGeneratorTests.cs
+++ tests/RoomSchedulerAPI.UnitTests/ServicesTests/TokenGeneratorTests.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using AutoFixture.Xunit2;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using Moq;
 using RoomSchedulerAPI.Features.Models.Entities;
@@ -14,7 +15,7 @@ public class TokenGeneratorTests
     private readonly Mock<IConfiguration> _configMock = new();
 
     public TokenGeneratorTests()
-    {        
+    {
         _configMock.Setup(c => c["Jwt:Issuer"]).Returns("TheIssuer");
         _configMock.Setup(c => c["Jwt:Audience"]).Returns("TheAudience");
         _configMock.Setup(c => c["Jwt:Key"]).Returns("TheSecretKeyIsNowGreaterThan256Bits");
@@ -22,68 +23,58 @@ public class TokenGeneratorTests
         _service = new TokenGenerator(_configMock.Object, _loggerMock.Object);
     }
 
-    [Fact]
-    public void GenerateToken_WhenUserIsNull_ThrowsArgumentException()
+    [Theory]
+    [AutoData]
+    public void GenerateToken_WhenUserIsNull_ThrowsArgumentException(List<UserRole> roles)
     {
-        // Arrange
-        List<UserRole> roles = [new UserRole { RoleName = "User" }];
-
-        // Act & Assert
-        var exception = Assert.Throws<ArgumentException>(() => _service.GenerateToken(null!, false, roles));
-        Assert.Equal("An authenticated user is needed", exception.Message); 
+        // Arrange, Act & Assert
+        var res = Assert.Throws<ArgumentException>(() => _service.GenerateToken(null!, false, roles));
+        Assert.Equal("An authenticated user is needed", res.Message);
     }
 
-    [Fact]
-    public void GenerateToken_WhenRolesAreNullOrEmpty_ThrowsArgumentException()
+    [Theory]
+    [AutoData]
+    public void GenerateToken_WhenRolesAreNullOrEmpty_ThrowsArgumentException(User user)
     {
-        // Arrange
-        var authenticatedUser = new User { Id = UserId.NewId, Email = "email@123@gmail.com" };
-
-        // Act & Assert
-        var exception = Assert.Throws<ArgumentException>(() => _service.GenerateToken(authenticatedUser, false, []));
-        Assert.Equal("Role(s) are needed", exception.Message);
+        // Arrange, Act & Assert
+        var res = Assert.Throws<ArgumentException>(() => _service.GenerateToken(user, false, []));
+        Assert.Equal("Role(s) are needed", res.Message);
     }
 
-    [Fact]
-    public void GenerateToken_WhenJwtKeyIsNotSet_ThrowsInvalidOperationException()
+    [Theory]
+    [AutoData]
+    public void GenerateToken_WhenJwtKeyIsNotSet_ThrowsInvalidOperationException(User user, List<UserRole> roles)
     {
         // Arrange
-        var authenticatedUser = new User { Id = UserId.NewId, Email = "email@123@gmail.com" };
-        List<UserRole> roles = [new UserRole { RoleName = "User" }];
-
         _configMock.Setup(c => c["Jwt:Key"]).Returns((string?)null);
 
         // Act & Assert
-        var exception = Assert.Throws<InvalidOperationException>(() => _service.GenerateToken(authenticatedUser, false, roles));
-        Assert.Equal("Missing configuration: Jwt:Key", exception.Message);
+        var res = Assert.Throws<InvalidOperationException>(() => _service.GenerateToken(user, false, roles));
+        Assert.Equal("Missing configuration: Jwt:Key", res.Message);
     }
 
-    [Fact]
-    public void GenerateToken_WhenJwtIssuerIsNotSet_ThrowsInvalidOperationException()
+    [Theory]
+    [AutoData]
+    public void GenerateToken_WhenJwtIssuerIsNotSet_ThrowsInvalidOperationException(User user, List<UserRole> roles)
     {
         // Arrange
-        var authenticatedUser = new User { Id = UserId.NewId, Email = "email@123@gmail.com" };
-        List<UserRole> roles = [new UserRole { RoleName = "User" }];
-
         _configMock.Setup(c => c["Jwt:Issuer"]).Returns((string?)null);
 
         // Act & Assert
-        var exception = Assert.Throws<InvalidOperationException>(() => _service.GenerateToken(authenticatedUser, false, roles));
-        Assert.Equal("Missing configuration: Jwt:Issuer", exception.Message);
+        var res = Assert.Throws<InvalidOperationException>(() => _service.GenerateToken(user, false, roles));
+        Assert.Equal("Missing configuration: Jwt:Issuer", res.Message);
     }
 
-    [Fact]
-    public void GenerateToken_WhenJwtAudienceIsNotSet_ThrowsInvalidOperationException()
+    [Theory]
+    [AutoData]
+    public void GenerateToken_WhenJwtAudienceIsNotSet_ThrowsInvalidOperationException(User user, List<UserRole> roles)
     {
         // Arrange
-        var authenticatedUser = new User { Id = UserId.NewId, Email = "email@123@gmail.com" };
-        List<UserRole> roles = [new UserRole { RoleName = "User" }];
-
         _configMock.Setup(c => c["Jwt:Audience"]).Returns((string?)null);
 
         // Act & Assert
-        var exception = Assert.Throws<InvalidOperationException>(() => _service.GenerateToken(authenticatedUser, false, roles));
-        Assert.Equal("Missing configuration: Jwt:Audience", exception.Message);
+        var res = Assert.Throws<InvalidOperationException>(() => _service.GenerateToken(user, false, roles));
+        Assert.Equal("Missing configuration: Jwt:Audience", res.Message);
     }
 
     [Fact]
@@ -140,8 +131,6 @@ public class TokenGeneratorTests
         var rolesClaim = jwtToken.Claims.Where(c => c.Type == ClaimTypes.Role).Select(r => r.Value).ToList();
         var rolesOutput = string.Join(", ", rolesClaim);
 
-        // Write to the console
-        Console.WriteLine($"Roles: {rolesOutput}");
         var passwordUpdatedClaim = jwtToken.Claims.FirstOrDefault(c => c.Type == "passwordUpdated");
 
         Assert.IsType<string>(result);
