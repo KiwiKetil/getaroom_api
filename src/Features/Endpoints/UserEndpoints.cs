565db01c3e188a418012cbf197f57e82778a1a20
KiwiKetil <ketilsveberg@gmail.com>
2025-03-19 12:45:41 +0100

    added and removed comments in userEndpoints.cs. Endret chained auth for noen endpoints

--- src/Features/Endpoints/UserEndpoints.cs
+++ src/Features/Endpoints/UserEndpoints.cs
@@ -19,19 +19,19 @@ public static class UserEndpoints
         .RequireAuthorization("AdminWithUpdatedPasswordPolicy")
         .WithName("GetUserById");
 
-        // https://localhost:7089/api/v1/users/b97ac10b-58cc-4372-a567-0e02b2c3d490 // jobb med og test denne først
+        // https://localhost:7089/api/v1/users/b97ac10b-58cc-4372-a567-0e02b2c3d490 
         app.MapPut("/api/v1/users/{id}", UserEndpointsLogic.UpdateUserLogicAsync)
-        .RequireAuthorization(/*"EmployeeOrAdminWithUpdatedPasswordPolicy"*/) // employees can only update clients. Admin can update all. Missing: Client can update self // change policy chained..
+        .RequireAuthorization() 
         .EndpointValidationFilter<UserUpdateDTO>()
         .WithName("UpdateUser");       
 
-        // https://localhost:7089/api/v1/users/6d7b1ca5-54f6-4859-a746-fc712d564128  // admin only // add so that admin cannot delete admin
+        // https://localhost:7089/api/v1/users/6d7b1ca5-54f6-4859-a746-fc712d564128 
         app.MapDelete("/api/v1/users/{id}", UserEndpointsLogic.DeleteUserLogicAsync)
         .RequireAuthorization("AdminWithUpdatedPasswordPolicy")
         .WithName("DeleteUser");
 
         // https://localhost:7089/api/v1/employees/register  // register employee, admin only // samel emd den under, kun 1 register? hmm.. when register employee - employee gets email to set own passsword!
-        app.MapPost("/api/v1/employees/register", UserEndpointsLogic.RegisterEmployeeLogicAsync)
+        app.MapPost("/api/v1/employees/register", UserEndpointsLogic.RegisterEmployeeLogicAsync) // husk if user already exist må kun throw ex alreadyexist dersom rollen er den samme.. 
         //.RequireAuthorization("AdminWithUpdatedPasswordPolicy")
         .EndpointValidationFilter<UserRegistrationDTO>()
         .WithName("RegisterEmployee");
